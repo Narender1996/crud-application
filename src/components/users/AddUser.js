c922e2815e910a508298d76c1bf6803740c719f3
narender bisht <narender007bisht@gmail.com>
2020-10-22 23:50:01 -0700

    first commit

---
+++ src/components/users/AddUser.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import axios from 'axios'
+import { useHistory } from "react-router-dom";
+
+const AddUser = () => {
+  let history = useHistory();
+  const [user, setUser] = useState({
+    name: "",
+    street: "",
+    city: "",  
+    state: "",
+    zipcode: ""
+  });
+
+  const { name, street, city, state, zipcode } = user;
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    await axios.post("http://localhost:3003/users", user);
+    history.push("/");
+  };
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Sender </h2>
+        <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Name"
+              name="name"
+              value={name}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your street"
+              name="street"
+              value={street}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your city"
+              name="city"
+              value={city}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your state"
+              name="state"
+              value={state}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Zip code"
+              name="zipcode"
+              value={zipcode}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <button className="btn btn-primary btn-block">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
---
+++ src/components/users/EditUser.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useHistory, useParams } from "react-router-dom";
+
+const EditUser = () => {
+  let history = useHistory();
+  const { id } = useParams();
+  const [user, setUser] = useState({
+    name: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: ""
+  });
+
+  const { name, street,city, state, zipcode } = user;
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    await axios.put(`http://localhost:3003/users/${id}`, user);
+    history.push("/");
+  };
+
+  const loadUser = async () => {
+    const result = await axios.get(`http://localhost:3003/users/${id}`);
+    setUser(result.data);
+  };
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Edit A User</h2>
+        <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Name"
+              name="name"
+              value={name}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your street"
+              name="street"
+              value={street}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your city"
+              name="city"
+              value={city}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your state"
+              name="state"
+              value={state}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your zipcode"
+              name="zipcode"
+              value={zipcode}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <button className="btn btn-warning btn-block">Update User</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditUser;
